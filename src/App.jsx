6d838fda7ae7a889c890235f6b101ba7957a5d38
Della Sari Siswanto <dsarisiswanto@gmail.com>
2026-03-17 09:01:16 +0700

    tampilan kalkulator pajak

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import KalkulatorPajak from "./components/KalkulatorPajak";
+
+export default function App() {
+    return (
+        <div>
+                <KalkulatorPajak />
+        </div>
+    );
+}
---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://103.163.37.26:3039/api",
+    headers: {
+        "Content-Type": "application/json",
+    }
+});
+
+export default api;
